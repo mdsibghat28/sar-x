5a683e7501138f442d7226dc7571d9cc15720c42
Sahith45 <sahithcherry99@gmail.com>
2021-10-22 19:26:48 +0530

    error solved

--- sar-x-angular/src/app/app.module.ts
+++ sar-x-angular/src/app/app.module.ts
@@ -5,38 +5,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
 import { CustomerNavbarComponent } from './customer-navbar/customer-navbar.component';
 import { TicketBookingComponent } from './ticket-booking/ticket-booking.component';
 import { SpaceRoomBookingComponent } from './space-room-booking/space-room-booking.component';
-=======
 import { LoginComponent } from './login/login.component';
 import { ViewBookingsComponent } from './view-bookings/view-bookings.component';
-import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
-import { CustomerNavbarComponent } from './customer-navbar/customer-navbar.component';
->>>>>>> rel1
 
 import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     HomeNavbarComponent,
     CustomerNavbarComponent,
     TicketBookingComponent,
-    SpaceRoomBookingComponent
-=======
-=======
+    SpaceRoomBookingComponent,
+
     RegistrationComponent,
->>>>>>> e8e414b116812081ccdcf397a139f9eff02fc2b6
+
     LoginComponent,
-    ViewBookingsComponent,
-    HomeNavbarComponent,
-    CustomerNavbarComponent
->>>>>>> rel1
+    ViewBookingsComponent
+    
   ],
   imports: [
     BrowserModule,
