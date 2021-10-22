99bb7c4cf4bdf995901b7390e352e028142dec83
Arshatullah Mohammed <arshuprivate@gmail.com>
2021-10-22 17:32:39 +0530

    view-bookings frontend implemented

--- sar-x-angular/src/app/app.module.ts
+++ sar-x-angular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewBookingsComponent } from './view-bookings/view-bookings.component';
+import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
+import { CustomerNavbarComponent } from './customer-navbar/customer-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewBookingsComponent
+    ViewBookingsComponent,
+    HomeNavbarComponent,
+    CustomerNavbarComponent
   ],
   imports: [
     BrowserModule,
