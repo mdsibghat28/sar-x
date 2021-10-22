c4127291173c149bd3045180079bad8b1ae5c8c0
radhasravani-16 <radhasravani.16@gmail.com>
2021-10-22 17:28:27 +0530

    login

--- sar-x-angular/src/app/app.module.ts
+++ sar-x-angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
+import { CustomerNavbarComponent } from './customer-navbar/customer-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeNavbarComponent,
+    CustomerNavbarComponent
   ],
   imports: [
     BrowserModule,
