3be11b5751c3323c59706e3e034a76290d9137b4
Arshatullah Mohammed <arshuprivate@gmail.com>
2021-10-22 22:59:59 +0530

    all project files merged

--- sar-x-angular/src/app/app.module.ts
+++ sar-x-angular/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
-import { CustomerNavbarComponent } from './customer-navbar/customer-navbar.component';
 import { TicketBookingComponent } from './ticket-booking/ticket-booking.component';
 import { SpaceRoomBookingComponent } from './space-room-booking/space-room-booking.component';
 import { LoginComponent } from './login/login.component';
 import { ViewBookingsComponent } from './view-bookings/view-bookings.component';
 
 import { RegistrationComponent } from './registration/registration.component';
+import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
+import { CustomerNavbarComponent } from './customer-navbar/customer-navbar.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +26,9 @@ import { RegistrationComponent } from './registration/registration.component';
     RegistrationComponent,
 
     LoginComponent,
-    ViewBookingsComponent
+    ViewBookingsComponent,
+    HomeNavbarComponent,
+    CustomerNavbarComponent
     
   ],
   imports: [
