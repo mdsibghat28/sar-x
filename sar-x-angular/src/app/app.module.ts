cd2affc307114c44ec0beb281eb3fbf270527c40
Arshatullah Mohammed <arshuprivate@gmail.com>
2021-10-22 16:24:31 +0530

    view booking component created

--- sar-x-angular/src/app/app.module.ts
+++ sar-x-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewBookingsComponent } from './view-bookings/view-bookings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewBookingsComponent
   ],
   imports: [
     BrowserModule,
