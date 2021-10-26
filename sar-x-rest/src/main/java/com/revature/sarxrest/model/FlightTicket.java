3709cf8dc33fc056e3ad937f00495c6a584cd73b
Sibghatullah <mdsibghat28@gmail.com>
2021-10-27 01:39:37 +0530

    merging all components

--- sar-x-rest/src/main/java/com/revature/sarxrest/model/FlightTicket.java
+++ sar-x-rest/src/main/java/com/revature/sarxrest/model/FlightTicket.java
@@ -18,90 +18,39 @@ public class FlightTicket {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="flightId")
 	private int flightId;
-	
 	@Column(name="name")
 	private String name;
-	
 	@Column(name="date")
 	private String date;
-	
 	@Column(name="className")
 	private String className;
 
-	@Column(name="price")
-	private double price;
-	
 	@Column(name="customerId")
 	private int customerId;
 	
 	@Column(name="passenger_name")
 	private String passenger_name;
-	
 	@Column(name="passengers")
 	private int passengers;
-	
 	@Column(name="source")
 	private String source;
-	
 	@Column(name="destination")
 	private String destination;
 	
-	public FlightTicket(String destination,String source,int passengers,String passenger_name,String name, String date, String className, double price,int customerId) {
 
+	
+	public FlightTicket(String name, String date, String className, int customerId, String passenger_name,
+			int passengers, String source, String destination) {
 		super();
-		this.customerId = customerId;
-//		this.bookingId = bookingId;
-		this.passenger_name =passenger_name;
-		this.passengers = passengers;
-		this.source = source;
-		this.destination = destination;
 		this.name = name;
 		this.date = date;
 		this.className = className;
-		this.price = price;
 		this.customerId = customerId;
-	}
-	
-	
-	public String getPassenger_name() {
-		return passenger_name;
-	}
-
-
-	public void setPassenger_name(String passenger_name) {
 		this.passenger_name = passenger_name;
-	}
-
-
-	public int getPassengers() {
-		return passengers;
-	}
-
-
-	public void setPassengers(int passengers) {
 		this.passengers = passengers;
-	}
-
-
-	public String getSource() {
-		return source;
-	}
-
-
-	public void setSource(String source) {
 		this.source = source;
-	}
-
-
-	public String getDestination() {
-		return destination;
-	}
-
-
-	public void setDestination(String destination) {
 		this.destination = destination;
 	}
-
 
 	public int getCustomerId() {
 		return customerId;
@@ -117,15 +66,6 @@ public class FlightTicket {
 		return flightId;
 	}
 	
-
-//	public int getBookingId() {
-//		return bookingId;
-//	}
-//
-//	public void setBookingId(int bookingId) {
-//		this.bookingId = bookingId;
-//	}
-
 	public void setFlightId(int flightId) {
 		this.flightId = flightId;
 	}
@@ -154,23 +94,47 @@ public class FlightTicket {
 		this.className = className;
 	}
 
-	
-	public double getPrice() {
-		return price;
+	public String getPassenger_name() {
+		return passenger_name;
 	}
 
-	public void setPrice(double price) {
-		this.price = price;
+	public void setPassenger_name(String passenger_name) {
+		this.passenger_name = passenger_name;
 	}
 
+	public int getPassengers() {
+		return passengers;
+	}
+
+	public void setPassengers(int passengers) {
+		this.passengers = passengers;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getDestination() {
+		return destination;
+	}
+
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
 
 	@Override
 	public String toString() {
-		return "FlightTicket [flightId=" + flightId + ", name=" + name + ", date=" + date + ", className=" + className
-				+ ", price=" + price + ", customerId=" + customerId + ", passenger_name=" + passenger_name
-				+ ", passengers=" + passengers + ", source=" + source + ", destination=" + destination + "]";
+		return "FlightTicket [name=" + name + ", date=" + date + ", className=" + className + ", customerId="
+				+ customerId + ", passenger_name=" + passenger_name + ", passengers=" + passengers + ", source="
+				+ source + ", destination=" + destination + "]";
 	}
-	
+
+
+
 	
 
 
